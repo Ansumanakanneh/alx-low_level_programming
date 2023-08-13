eb171d41213b25a03cde7e025d5e40269aaa6a5b
Ansumanakanneh <ansumanakanneh@gmail.com>
2023-08-13 10:35:03 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point of a program
  *
- * Return: always 0
+ * Return: 0 always
  */
 
 int main(void)
 {
 	int i;
 	char j;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
