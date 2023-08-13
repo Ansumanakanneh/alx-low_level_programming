cce6daed3ebd42c963a35a63ab107366eca69a2c
Ansumanakanneh <ansumanakanneh@gmail.com>
2023-08-13 13:16:11 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 99 ; i++)
+	{
+		for (j = i ; j <= 99 ; j++)
+		{
+			if (i == 1 && j == 0)
+			{
+			printf("%02d %02d", i, j);
+			if (j < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
